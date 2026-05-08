e0acb22aff6e4a306ea6c711aea6c4e6db8213c3
liamCodes123 <604729liam@gmail.com>
2026-05-08 11:17:41 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.body.style.overflow = "auto";
+    }, 3500);
+});
 
+function showMessage(){
+    alert("Welcome to the United Lands of Helbig!");
+}
